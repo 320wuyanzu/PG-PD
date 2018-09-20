bb53af2683fb0b6a3aefb134e181a3d74e31df6b
wind <windshear222@outlook.com>
2018-09-20 19:20:09 +0800

    先来个考纲

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,138 @@
+module.exports = {
+    base: '/PG-PD/',
+    title: '程序设计(F01)考试大纲',
+    themeConfig: {
+        lastUpdated: '最后一次编辑',
+        nav: [
+            { text: 'Personal Homepage', link: 'https://320wuyanzu.github.io' },
+            { text: 'Home', link: '/' },
+        ],
+        sidebarDepth: 2,
+        //sidebar: sidebar_group()
+    }
+}
+
+
+/**
+ * 折叠式侧边栏
+ */
+/*
+function sidebar_group() {
+    let ATA21 = {
+        title: 'ATA21-Air Conditioning',
+        children: []
+    };
+    let ATA22 = {
+        title: 'ATA22-Auto Flight',
+        children: []
+    };
+    let ATA23 = {
+        title: 'ATA23-Communications',
+        children: []
+    };
+    let ATA24 = {
+        title: 'ATA24-Electrical Power',
+        children: []
+    };
+    let ATA25 = {
+        title: 'ATA25-Equipment/Furnishings',
+        children: []
+    };
+    let ATA26 = {
+        title: 'ATA26-Fire Protection',
+        children: []
+    };
+    let ATA27 = {
+        title: 'ATA27-Flight Controls',
+        children: []
+    };
+    let ATA28 = {
+        title: 'ATA28-Fuel',
+        children: []
+    };
+    let ATA29 = {
+        title: 'ATA29-Hydraulic Power',
+        children: []
+    };
+    let ATA30 = {
+        title: 'ATA30-Ice/Rain Protection',
+        children: []
+    };
+    let ATA31 = {
+        title: 'ATA31-Indicating/Recording',
+        children: []
+    };
+    let ATA32 = {
+        title: 'ATA32-Landing Gear',
+        children: []
+    };
+    let ATA33 = {
+        title: 'ATA33-Lights',
+        children: []
+    };
+    let ATA34 = {
+        title: 'ATA34-Navigation',
+        children: []
+    };
+    let ATA35 = {
+        title: 'ATA35-Oxygen',
+        children: []
+    };
+    let ATA36 = {
+        title: 'ATA36-Pneumatic',
+        children: []
+    };
+    let ATA38 = {
+        title: 'ATA38-Water/Waste',
+        children: []
+    };
+    let ATA46 = {
+        title: 'ATA46-Information Systems',
+        children: []
+    };
+    let ATA47 = {
+        title: 'ATA47-Inert Gas System',
+        children: []
+    };
+    let ATA49 = {
+        title: 'ATA49-APU',
+        children: []
+    };
+    let ATA51 = {
+        title: 'ATA51-Structures',
+        children: []
+    };
+    let ATA52 = {
+        title: 'ATA52-Doors',
+        children: []
+    };
+    let ATA71 = {
+        title: 'ATA71-Power Plant(CFM56)',
+        children: []
+    };
+    return [
+        ATA21,
+        ATA22,
+        ATA23,
+        ATA24,
+        ATA25,
+        ATA26,
+        ATA27,
+        ATA28,
+        ATA29,
+        ATA30,
+        ATA31,
+        ATA32,
+        ATA33,
+        ATA34,
+        ATA35,
+        ATA36,
+        ATA38,
+        ATA46,
+        ATA47,
+        ATA49,
+        ATA51,
+        ATA52,
+        ATA71
+    ]
+}*/
